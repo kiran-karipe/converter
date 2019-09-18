e4c201022659fb59274ba76d1594581afa95278b
Kiran <kiran.karipe@gmail.com>
2019-09-18 01:15:48 -0700

    Added state and event handlers to the components

--- src/components/converter/Converter.js
+++ src/components/converter/Converter.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Header from '../header/Header';
+import { Container } from 'reactstrap';
 
 class Converter extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            converter: ''
+        };
+        this.onNavSubmit = this.onNavSubmit.bind(this);
+    }
+
+    onNavSubmit(converter) {
+        this.setState({ converter });
+    }
     render() {
         let appConverters = ['Currency', 'Weight', 'Temperature', 'Time', 'Length'];
         return (
             <div>
-                <Header converters={appConverters}/>
+                <Header converters={appConverters} onClick={this.onNavSubmit}/>
+                <Container>{this.state.converter}</Container>
             </div>
         );
     }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,8 +3,24 @@ import { Button } from 'reactstrap';
 
 class Header extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            converter: 'Currency'
+        }
+        this.submitConverter = this.submitConverter.bind(this);
+    }
+
+    submitConverter(converter) {
+        this.setState({converter: converter});
+        const { onClick } = this.props;
+
+        onClick(converter);
+    }
+
     renderConverter(converter, id) {
-        return <Button color="primary" key={id}>{converter}</Button>
+        return <Button color="primary" value={converter} key={id} onClick={() => this.submitConverter(converter)}>{converter}</Button>
     }
 
     render() {
