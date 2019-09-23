013ba9976ec711e9c04dea46537f5bc68c4a3237
Kiran <kiran.karipe@gmail.com>
2019-09-22 22:29:45 -0700

    Added input fields

--- src/components/convert/Convert.js
+++ src/components/convert/Convert.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Input, Media } from 'reactstrap';
 import './Convert.css';
 
 class Convert extends React.Component {
@@ -29,35 +29,46 @@ class Convert extends React.Component {
 
     render() {
         return (
-            <Container className="Converter">
-                <h1>{this.props.converter}</h1>
-                <Row className="row-padding-top">
-                    <Col sm="6">
-                        <Dropdown isOpen={this.state.dropdownOpen_1} toggle={this.toggle_1}>
-                            <DropdownToggle caret>
-                                {this.props.currencies[0]}
-                                <DropdownMenu>
-                                    {this.props.currencies.map((currency, id) => (
-                                        <DropdownItem value={currency} key={id}>{currency}</DropdownItem>
-                                    ))}
-                                </DropdownMenu>
-                            </DropdownToggle>
-                        </Dropdown>
-                    </Col>
-                    <Col sm="6">
-                        <Dropdown isOpen={this.state.dropdownOpen_2} toggle={this.toggle_2}>
-                            <DropdownToggle caret>
-                                {this.props.currencies[1]}
-                                <DropdownMenu>
-                                    {this.props.currencies.map((currency, id) => (
-                                        <DropdownItem value={currency} key={id}>{currency}</DropdownItem>
-                                    ))}
-                                </DropdownMenu>
-                            </DropdownToggle>
-                        </Dropdown>
-                    </Col>
-                </Row>
-            </Container>
+            <div className="padding-top">
+                <Container className="Converter padding-bottom">
+                    <h1>{this.props.converter}</h1>
+                    <Row className="row-padding-top">
+                        <Col xs="6" sm="4">
+                            <Dropdown isOpen={this.state.dropdownOpen_1} toggle={this.toggle_1}>
+                                <DropdownToggle caret>
+                                    {this.props.currencies[0]}
+                                    <DropdownMenu>
+                                        {this.props.currencies.map((currency, id) => (
+                                            <DropdownItem value={currency} key={id}>{currency}</DropdownItem>
+                                        ))}
+                                    </DropdownMenu>
+                                </DropdownToggle>
+                            </Dropdown>
+                            <div className="padding-top">
+                                <Input placeholder="enter amount"/>
+                            </div>
+                        </Col>
+                        <Col sm="4">
+                            <Media object data-src="https://www.westernunion.com/etc/designs/westernunion/responsive_css/images/icon-smo-double-arrow.svg" />
+                        </Col>
+                        <Col xs="6" sm="4">
+                            <Dropdown isOpen={this.state.dropdownOpen_2} toggle={this.toggle_2}>
+                                <DropdownToggle caret>
+                                    {this.props.currencies[1]}
+                                    <DropdownMenu>
+                                        {this.props.currencies.map((currency, id) => (
+                                            <DropdownItem value={currency} key={id}>{currency}</DropdownItem>
+                                        ))}
+                                    </DropdownMenu>
+                                </DropdownToggle>
+                            </Dropdown>
+                            <div className="padding-top">
+                                <Input placeholder="enter amount"/>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
